3e794c636f14f736f508a865dda4b919099d9da2
Jaehyung-Dev <publicdev2024@gmail.com>
2025-02-12 15:11:58 +0900

    Create Vue Project

---
+++ src/assets/movies.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+      title: '노랑',
+      year: 2023,
+      category: '액션, 드라마',
+      textRed: 'color: red',
+      like: 0,
+      imgUrl: './assets/minji1.jpg'
+    },
+    {
+      title: '아쿠아맨과 로스트 킹덤',
+      year: 2023,
+      category: '액션, 판타지, 어드밴처',
+      like: 0,
+      imgUrl: './assets/minji2.jpg'
+    },
+    {
+      title: '3일의 휴가',
+      year: 2023,
+      category: '판타지, 드라마',
+      like: 0,
+      imgUrl: './assets/wonyeong.jpg'
+    },
+  ]
+
+export default data;
